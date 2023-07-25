03040dcfbc87b57740f940a835deb87d7c199f98
neelansh27 <neelanshsharma30@gmail.com>
2023-07-25 12:04:38 +0530

    Login page is now working

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, redirect, render_template, request, url_for,session
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt, bcrypt
+from flask_login import LoginManager, current_user, login_required, login_user, logout_user , UserMixin
 import os
+
+from sqlalchemy.engine import url
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -11,7 +14,14 @@ app.secret_key = 'your_secret_key'
 
 db = SQLAlchemy(app)
 bcrypt=Bcrypt(app)
-class data(db.Model):
+login_manager= LoginManager(app)
+login_manager.login_view='index' # reffering to login page
+
+@login_manager.user_loader 
+def load_user(user_id):
+    return data.query.get(int(user_id))
+
+class data(db.Model,UserMixin):
     id = db.Column(db.Integer, primary_key=True,autoincrement=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -21,21 +31,29 @@ class data(db.Model):
         return f'Name:{self.username}'
 
 #FOLLOWING CODE CAN BE RUN ONCE TO CREATE THE DATABASE
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-@app.route('/')
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()
+
+@app.route('/') #redirecting root to a different page
 def re():
     return redirect(url_for('index'))
+
 @app.route('/index',methods=['GET','POST'])
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('success'))
     if request.method=='POST':
         email=request.form.get("email")
-        existing_user = data.query.filter_by(email=email).first()
-        if not existing_user:
+        password=request.form.get('password')
+        user = data.query.filter_by(email=email).first()
+        if not user: # checking if user exists of not
             return render_template('index.html',error='User does not exists')
-        session['logged_in']=True
-        return redirect(url_for('success'))
+        if bcrypt.check_password_hash(user.password,password):
+            login_user(user) 
+            return redirect(url_for('success'))
+        else:
+            return render_template('index.html',error='Incorrect password')
     return render_template("index.html")
 
 @app.route('/sign-up',methods=['GET','POST'])
@@ -60,9 +78,29 @@ def signup():
     return render_template("signup.html")
 
 @app.route('/success')
+@login_required
 def success():
-    if session.get('logged_in',None) is None:
-        return redirect(url_for('index'))
-    return render_template('success.html')
+    return render_template('success.html', user=current_user)
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
